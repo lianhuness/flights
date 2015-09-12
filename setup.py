c68aaf9cabe7624f90939efbe60782f6da6dbc2b
zcnetwork <zerocoolnetwork@gmail.com>
2015-09-12 00:56:47 -0400

    change stup

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ setup(
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4'
+        'Django==1.8.4',
+        'requests',
+        'beautifulsoup4',
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
