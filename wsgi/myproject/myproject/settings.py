c3257ce69c0149045ec0cb415dae6850588933d5
zcnetwork <zerocoolnetwork@gmail.com>
2015-09-13 04:18:08 -0400

    remove registration

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'flights',
+    # 'django.contrib.sites',
+    # 'registration',
 )
 
 MIDDLEWARE_CLASSES = (
