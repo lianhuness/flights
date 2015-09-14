f9245f528b09d4c855e0485644839a472963650b
zcnetwork <zerocoolnetwork@gmail.com>
2015-09-13 22:45:54 -0400

    remove b

--- wsgi/myproject/flights/migrations/0001_initial.py
+++ wsgi/myproject/flights/migrations/0001_initial.py
@@ -16,9 +16,9 @@ class Migration(migrations.Migration):
             name='Subscriber',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('status', models.CharField(default=b'INACTIVE', max_length=b'10')),
-                ('fromAirport', models.CharField(max_length=b'100')),
-                ('toAirport', models.CharField(max_length=b'100')),
+                ('status', models.CharField(default='INACTIVE', max_length='10')),
+                ('fromAirport', models.CharField(max_length='100')),
+                ('toAirport', models.CharField(max_length='100')),
                 ('startDate', models.DateField()),
                 ('endDate', models.DateField()),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
